088308fc057b223d27f9705720802358385ce727
liyichun0923 <569275210@qq.com>
2019-11-14 08:42:54 +0800

    zhuba

---
+++ build-zhuba/precache-manifest.f45e6724809e9af2e7ba5625975ba070.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aef0ec393daae36b7811bf7bfccb564",
+    "url": "index.html"
+  },
+  {
+    "revision": "d08aa6e41a3bc35c0430",
+    "url": "static/css/2.5bff5e6d.chunk.css"
+  },
+  {
+    "revision": "18439b4451aaff3daf39",
+    "url": "static/css/main.0c8d0ccc.chunk.css"
+  },
+  {
+    "revision": "d08aa6e41a3bc35c0430",
+    "url": "static/js/2.ce61171f.chunk.js"
+  },
+  {
+    "revision": "18439b4451aaff3daf39",
+    "url": "static/js/main.5b63d071.chunk.js"
+  },
+  {
+    "revision": "067790caabdba6a1564e",
+    "url": "static/js/runtime-main.d10b7575.js"
+  },
+  {
+    "revision": "5c882bdb23c1764780e64a5e3388bbf4",
+    "url": "static/media/di.5c882bdb.png"
+  },
+  {
+    "revision": "b9757a06f5d9e05ee625df83a95b4e82",
+    "url": "static/media/linggan.b9757a06.png"
+  },
+  {
+    "revision": "2dc362179cb153849fc61a60503eddfe",
+    "url": "static/media/linggan1.2dc36217.png"
+  },
+  {
+    "revision": "1696c81e7edfe8ffd3a35a270bfd7535",
+    "url": "static/media/s1.1696c81e.png"
+  },
+  {
+    "revision": "443e971588b42f9b768a836334b6958a",
+    "url": "static/media/s2.443e9715.png"
+  },
+  {
+    "revision": "c531d016a550bdca0070921a1c13c2c9",
+    "url": "static/media/shang.c531d016.png"
+  },
+  {
+    "revision": "128f22e9e49586c2f16c48f2857e90db",
+    "url": "static/media/tui1.128f22e9.png"
+  },
+  {
+    "revision": "59bf8321508b42fc537b200b6ec52f8d",
+    "url": "static/media/tui2.59bf8321.png"
+  },
+  {
+    "revision": "becabd8032f8d613749594d569f558ff",
+    "url": "static/media/tui3.becabd80.png"
+  },
+  {
+    "revision": "a0cdd2eb336b953ad68c6545b7c22368",
+    "url": "static/media/tui4.a0cdd2eb.png"
+  },
+  {
+    "revision": "55cadb27a8e570353152df60893616e2",
+    "url": "static/media/wode.55cadb27.png"
+  },
+  {
+    "revision": "c41e5237ccc4f4348c42d0b2de7a722c",
+    "url": "static/media/wode1.c41e5237.png"
+  },
+  {
+    "revision": "fb28c3626b519c71ffd18bb7672fb33d",
+    "url": "static/media/住1.fb28c362.jpg"
+  },
+  {
+    "revision": "8bc4856ae322118d834557430db1bc17",
+    "url": "static/media/住2.8bc4856a.jpg"
+  },
+  {
+    "revision": "d9cfe5bec466795bb0376b624c5693c1",
+    "url": "static/media/住3.d9cfe5be.jpg"
+  },
+  {
+    "revision": "4ae06836c877edd6ce37da4f76b98240",
+    "url": "static/media/住6.4ae06836.jpg"
+  }
+]);
